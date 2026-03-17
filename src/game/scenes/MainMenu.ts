dda49139876848e88f32840a96fd7006045d5138
= <=>
2026-03-17 15:20:53 +0100

    Add Scene Switch per Click in Main Menu

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -16,17 +16,21 @@ export class MainMenu extends Scene
 
     create ()
     {
-        //this.background = this.add.image(512, 384, 'background');
         this.background = this.add.image(768,512, 'mm_background');
         this.logo = this.add.image(768,512, 'logo');
 
-        this.title = this.add.text(200, 100, 'Start Game', {
+        this.title = this.add.text(768, 1000, 'Click anywhere to Start', {
             fontFamily: 'actionman', fontSize: 60, color: '#ff9000',
             stroke: '#893700', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
 
         EventBus.emit('current-scene-ready', this);
+
+        // Listener für Maus-Klick 
+        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+            this.scene.start('Game');
+        });
     }
     
     changeScene ()
